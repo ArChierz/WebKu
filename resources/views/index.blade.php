204c24a4e77265383fcb18d65ecdabe61fcc6e8a
Muhammad Zidan Ramadhan <zidan.ramadhan102@gmail.com>
2025-05-15 10:27:47 +0700

    second commit cuz forgor

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web baruku</title>
+</head>
+<body>
+    <div>
+        <h1 style="text-align: center">WeBkU</h1>
+        <figure>
+            <img src="{{ url('/img/forgor.jpg') }}" alt="forgor" width="10%" style="display:block; margin-left: auto; margin-right: auto;" >
+            <figcaption style="text-align:center">Forgor</figcaption>
+            <audio controls loop style="margin: 0 auto; display: block;">
+                <source src="{{ url('/audio/アホの子.mp3') }}" type="audio/mpeg">
+                </audio>
+            </figure>
+            
+        <figure>
+            <figcaption style="text-align:center; font-family: 'Plus Jakarta Sans', sans-serif; font-style: normal; font-size: large;">Check My Oshi Talent here!</figcaption> <br>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/kgCmpVtEx54?si=Bd5so74mt77_PtZ1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="display:block; margin-left: auto; margin-right: auto;"></iframe>
+        </figure>
+        
+    </div>
+</body>
+</html>
